9cf3d035f1b1577a05e2c41224350f63da011efd
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-08-10 14:58:40 -0700

    Add src and top level for class scanning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./dist/**/*.{html,js}"
+    "./src/**/*.{html,js}",
+    "./dist/**/*.{html,js}",
+    "./*.{html,js}"
   ],
   theme: {
     extend: {},
