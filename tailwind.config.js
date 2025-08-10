22586150c1c3bce6e8b38c7588989323e8b25a3e
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-08-10 16:37:45 -0700

    Scan dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./src/**/*.{html,js}",
+    "./dist/**/*.{html,js}"
   ],
   theme: {
     extend: {},
   },
   plugins: [require('daisyui')],
 }
-
