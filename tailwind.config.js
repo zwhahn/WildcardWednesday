565638ab6b04c786d6f9ac05c0f563d70e7aab4d
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-08-10 15:12:44 -0700

    Remove additional content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
     "./dist/**/*.{html,js}",
-    "./*.{html,js}"
   ],
   theme: {
     extend: {},
